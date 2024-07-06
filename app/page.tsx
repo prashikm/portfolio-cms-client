808cb6b2ca6bdd05755ce13c51b987faf4df22f6
prashikm <prashikm861@gmail.com>
2024-07-06 13:31:28 +0530

    update landing page

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,21 @@
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+
 export default async function Home() {
   return (
-    <main className="mt-20 mb-56 max-w-4xl mx-auto px-6 text-center">
-      <h1>Make your portfolio and showcase your work to the world.</h1>
+    <main className="mt-20 mb-56 max-w-2xl mx-auto px-6 text-center">
+      <h1 className="text-3xl font-bold md:text-4xl">
+        Make your portfolio and showcase your work to the world.
+      </h1>
+      <div className="mt-10 flex justify-center gap-6 items-center">
+        <Button variant="outline" asChild>
+          <Link href="/login">Login</Link>
+        </Button>
+        or
+        <Button asChild>
+          <Link href="/signup">Sign up</Link>
+        </Button>
+      </div>
     </main>
   );
 }
